3a6297a6cc383e644ad045bfce40700ac3635ac4
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2024-05-21 10:07:22 +0000

    feat(lab01): object and array

---
+++ lab01/demo04.js
@@ -0,0 +1,14 @@
+// define object and array
+
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+
+let obj = {
+    name: "John",
+    age: 25,
+    address: "SN"
+};
+console.log(obj);
+console.log(obj.name);
+
+console.log(JSON.stringify(obj));
