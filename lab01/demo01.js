79d2fd0a0052c185e843844220318cd51ece9295
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2024-05-21 09:46:51 +0000

    feat(lab01): variable

---
+++ lab01/demo01.js
@@ -0,0 +1,19 @@
+// variable
+
+let a = 1;
+console.log(a);
+
+a = 'Hello world'
+console.log(a);
+
+// local varaible
+let b = 10;
+console.log('Local variable b = ', b);
+
+// global variable
+var c = 'Hello world';
+console.log('Global variable c = ', c);
+
+// constant
+const d = 'Hello Constant';
+console.log('Constant d = ', d);
